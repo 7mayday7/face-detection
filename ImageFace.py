dd98d43d400879dc85828819117d05c89cd62376
denis <stroodenis462@mail.ru>
2023-04-11 12:18:23 +0600

    Initial commit

---
+++ ImageFace.py
@@ -0,0 +1,17 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+img = cv2.imread('test_img.jpg')
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(img_gray)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 169, 244), 1)
+
+
+cv2.imshow('Test', img)
+
+cv2.waitKey(0)
---
+++ VideoCamFace.py
@@ -0,0 +1,18 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img_gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 168, 244), 1)
+
+    cv2.imshow('Test', frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
